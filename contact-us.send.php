939686bfd30474f580b9191366f610d795e504a1
vanhnl <46739257+vanhnl@users.noreply.github.com>
2019-12-23 16:15:17 +0700

    abc

--- contact-us.send.php
+++ contact-us.send.php
@@ -1,17 +1,12 @@
 <?php
-	header('Content-type: application/json');
-	$status=array(
-		'type'=>'success',
-		'message'=>'Email sent!'
-	);
-    $name=@trim(stripslashes($_POST['name']));
-    $email=@trim(stripslashes($_POST['email']));
-    $phone=@trim(stripslashes($_POST['phone']));
-    $message=@trim(stripslashes($_POST['message']));
-    $email_from=$email;
-    $email_to='company@domain.com';
-    $body='Name: '.$name."\n\n".'Email: '.$email."\n\n".'Telephone: '.$phone."\n\n".'Message: '.$message;
-    mail($email_to, 'Contact form', $body, 'From: <'.$email_from.'>');
-    echo json_encode($status);
-    die;
+	$dbhost='localhost';
+	$db=''
+	$fullname='root';
+	$email='';
+	$telephone='';
+	$message='';
+
+	mysqli_connect("$dbhost","$fullname","$email","$telephone","$message");
+	echo"asdjkf";
+	mysqli_select_db($db);
 ?>
